f0c2dacbaabf793f4e93e3128abd3c3a03098957
data-sign <77310679+data-sign@users.noreply.github.com>
2023-04-03 17:22:55 +0900

    [Unrated] Title: 큐 2, Time: 1604 ms, Memory: 175176 KB -BaekjoonHub

---
+++ 백준/Unrated/18258. 큐 2/큐 2.py
@@ -0,0 +1,30 @@
+## 10258 : 큐2
+from collections import deque
+import sys
+N = int(sys.stdin.readline())
+q = deque()
+
+for _ in range(N): 
+  cmd = sys.stdin.readline().split()
+  if cmd[0] == 'push': 
+    q.append(cmd[1])
+  elif cmd[0] == 'pop':
+    if q:
+      print(q.popleft())
+    else: 
+      print(-1)
+  elif cmd[0] == 'size':
+    print(len(q))
+  elif cmd[0] == 'empty':
+    if q: print(0)
+    else: print(1)
+  elif cmd[0] == 'front':
+    if q:
+      print(q[0])
+    else: 
+      print(-1)
+  elif cmd[0] == 'back':
+    if q: 
+      print(q[-1])
+    else: 
+      print(-1)
