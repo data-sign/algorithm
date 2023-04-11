bbc68ff80bd7c927ecc1004b8fa742fb31232091
data-sign <77310679+data-sign@users.noreply.github.com>
2023-04-11 15:10:18 +0900

    [Silver I] Title: 하노이 탑, Time: 956 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,16 @@
+import sys
+def HANOI(x, y, z, cnt):
+    if cnt == 0:
+        return
+    # n-1 번째 원반 두번째 기둥으로 옮겨야 함 
+    HANOI(x, z, y, cnt - 1)
+    # 가장 큰 크기 원반 출력 1->3
+    print(x,z)
+    # n-1번째 원반들을 3번째로 옮겨야 함 
+    HANOI(y, x, z, cnt - 1)
+
+N = int(sys.stdin.readline())
+print((1 << N) - 1)
+# print(2 ** N - 1)
+if N <= 20:
+    HANOI(1, 2, 3, N)
