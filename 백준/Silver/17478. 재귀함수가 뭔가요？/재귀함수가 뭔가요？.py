0109db06b884139c7c9676a3bf5e95365cce5ebc
data-sign <77310679+data-sign@users.noreply.github.com>
2023-04-11 13:05:38 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
@@ -0,0 +1,28 @@
+cnt = int(input())
+a = "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다."
+b = "\"재귀함수가 뭔가요?\""
+c = "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어."
+d = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지."
+e = "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\""
+f = "라고 답변하였지."
+g = "\"재귀함수는 자기 자신을 호출하는 함수라네\""
+
+under_bar = '____'
+
+def answer(n):
+  
+  if n == 0:
+    print(under_bar*(cnt-n) + b)
+    print(under_bar*(cnt-n) + g)
+    print(under_bar*(cnt-n) + f) 
+    return
+  else: 
+    print(under_bar*(cnt-n) + b)
+    print(under_bar*(cnt-n) + c)
+    print(under_bar*(cnt-n) + d)
+    print(under_bar*(cnt-n) + e)
+  answer(n-1)
+  print(under_bar*(cnt-n) + f)
+
+print(a) 
+answer(cnt)
