1376bc50b8efe6f896433283fc0213724aa9b9db
data-sign <77310679+data-sign@users.noreply.github.com>
2023-05-16 13:55:56 +0900

    [Silver II] Title: 종이의 개수, Time: 4912 ms, Memory: 69432 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -0,0 +1,38 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+paper = [list(map(int, input().split())) for _ in range(n)]
+
+answer = [0,0,0]
+
+def dfs(x, y, n):
+    """x, y : 좌표값, n : 종이의 차원"""
+    global answer 
+    
+    # 종이의 시작점 값 = visited 
+    visited = paper[x][y]
+    # 반복문을 통해 종이를 확인
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            # 종이의 시작점 값(0,-1,1)과 다른 값과 비교 
+            if paper[i][j] != visited:
+                # 다르면 3으로 나눈 범위로(3*3) x,y 좌표 재귀적으로 탐색 
+                for k in range(3): 
+                    for l in range(3): 
+                        # k = 0 : (0 + (0*3), 0+(0*3), 3), (0+(0*3), 0+(1*3), 3), (0+(0*3), 0+(2*3), 3) = (0,0,3), (0,3,3), (0,6,3)
+                        # k = 1 : (0 + (1*3), 0+(0*3), 3) .... = (3, 0, 3), (3, 3, 3), (3, 6, 3)
+                        dfs(x + k*(n//3), y + l*n//3, n//3)
+                return
+
+    # 카운트
+    if visited == -1:
+        answer[0] += 1
+    elif visited == 0:
+        answer[1] += 1
+    else:
+        answer[2] += 1
+
+dfs(0, 0, n)
+print(*answer, sep='\n')
