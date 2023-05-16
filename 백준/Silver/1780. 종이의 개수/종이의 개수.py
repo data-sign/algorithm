46a47a39ad0d535f62357c7dfb55effd16b52cbf
data-sign <77310679+data-sign@users.noreply.github.com>
2023-05-16 14:00:00 +0900

    [Silver II] Title: 종이의 개수, Time: 4352 ms, Memory: 69500 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -10,7 +10,7 @@ answer = [0,0,0]
 def dfs(x, y, n):
     """x, y : 좌표값, n : 종이의 차원"""
     global answer 
-    
+
     # 종이의 시작점 값 = visited 
     visited = paper[x][y]
     # 반복문을 통해 종이를 확인
@@ -19,11 +19,24 @@ def dfs(x, y, n):
             # 종이의 시작점 값(0,-1,1)과 다른 값과 비교 
             if paper[i][j] != visited:
                 # 다르면 3으로 나눈 범위로(3*3) x,y 좌표 재귀적으로 탐색 
-                for k in range(3): 
-                    for l in range(3): 
-                        # k = 0 : (0 + (0*3), 0+(0*3), 3), (0+(0*3), 0+(1*3), 3), (0+(0*3), 0+(2*3), 3) = (0,0,3), (0,3,3), (0,6,3)
-                        # k = 1 : (0 + (1*3), 0+(0*3), 3) .... = (3, 0, 3), (3, 3, 3), (3, 6, 3)
-                        dfs(x + k*(n//3), y + l*n//3, n//3)
+                # for k in range(3): 
+                #     for l in range(3): 
+                #         # k = 0 : (0 + (0*3), 0+(0*3), 3), (0+(0*3), 0+(1*3), 3), (0+(0*3), 0+(2*3), 3) = (0,0,3), (0,3,3), (0,6,3)
+                #         # k = 1 : (0 + (1*3), 0+(0*3), 3) .... = (3, 0, 3), (3, 3, 3), (3, 6, 3)
+                #         dfs(x + k*(n//3), y + l*n//3, n//3)
+
+                # 재귀대신 경우의 수 나열 
+                next_n = n//3
+                dfs(x, y, next_n) 
+                dfs(x+next_n, y, next_n) 
+                dfs(x+(next_n*2), y, next_n) 
+                dfs(x, y+next_n, next_n) 
+                dfs(x, y+(next_n*2), next_n) 
+                dfs(x+next_n, y+next_n, next_n) 
+                dfs(x+(next_n*2), y+next_n, next_n) 
+                dfs(x+next_n, y+(next_n*2), next_n) 
+                dfs(x+(next_n*2), y+(next_n*2), next_n) 
+
                 return
 
     # 카운트
