29872a9fac07fefc7605ee5ac17845851c4ef54a
data-sign <77310679+data-sign@users.noreply.github.com>
2023-07-09 02:30:57 +0900

    [Gold V] Title: 이진 검색 트리, Time: 2380 ms, Memory: 33684 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -0,0 +1,42 @@
+import sys
+
+# default 값이 1000이다
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+
+def post_order(start, end):
+    # 역전시 리턴
+    if start > end:
+        return
+
+    # 루트 노드
+    root = pre_order[start]
+    idx = start + 1
+
+    # root보다 커지는 지점을 찾는 과정  
+    # for문으로 찾으면 안된다. 아래서 설명  
+    while idx <= end:
+        if pre_order[idx] > root:
+            break
+        idx += 1
+
+    # 왼쪽 서브트리
+    post_order(start + 1, idx - 1)
+
+    # 오른쪽 서브트리
+    post_order(idx, end)
+
+    # 후위순회이므로 제일 마지막에 찍으면 된다
+    print(root)
+
+
+pre_order = []
+while 1:
+    try:
+        pre_order.append(int(input()))
+    # try에서 예외 발생시 break 실행
+    except:
+        break
+
+post_order(0, len(pre_order) - 1)
