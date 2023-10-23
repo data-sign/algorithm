66e3855d82b144879f84776c5edff7c14257aaf5
data-sign <77310679+data-sign@users.noreply.github.com>
2023-10-23 22:25:22 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 1208 ms, Memory: 110608 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,32 +1,32 @@
-# 백트래킹 (Python3 통과, PyPy3도 통과)
 import sys
-
 input = sys.stdin.readline
+
 N = int(input())
-num = list(map(int, input().split()))
-op = list(map(int, input().split()))  # +, -, *, //
+num_lst = list(map(int, input().split()))
+plus, minus, mul, div = map(int, input().split())
+from itertools import permutations
 
-maximum = -1e9
-minimum = 1e9
-
-
-def dfs(depth, total, plus, minus, multiply, divide):
-    global maximum, minimum
-    if depth == N:
-        maximum = max(total, maximum)
-        minimum = min(total, minimum)
-        return
-
-    if plus:
-        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
-    if minus:
-        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
-    if multiply:
-        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
-    if divide:
-        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
-
-
-dfs(1, num[0], op[0], op[1], op[2], op[3])
-print(maximum)
-print(minimum)
+min_, max_ =  int(1e9), -int(1e9)
+operator = '+'*plus + '-'*minus + '*'*mul + '/'*div
+for o in permutations(operator, N-1): 
+    #print(o)
+    num = num_lst[0]
+    for i in range(1, len(num_lst)): 
+        if o[i-1] == '+': 
+            num += num_lst[i]
+        elif o[i-1] == '*': 
+            num *= num_lst[i]
+        elif o[i-1] == '-': 
+            num -= num_lst[i]
+        elif o[i-1] == '/':
+            if num < 0 and num_lst[i] > 0:
+                num = -1*(num*(-1)//num_lst[i])
+            else: 
+                num = num//num_lst[i]
+        #print(num)
+    
+    if max_ < num: 
+        max_ = num
+    if min_ > num: 
+        min_ = num
+print(max_, min_, sep='\n')
