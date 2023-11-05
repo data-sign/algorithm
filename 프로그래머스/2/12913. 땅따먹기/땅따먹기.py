d5ac7699eba435e3ab3e7c615361eeda47d279b3
data-sign <77310679+data-sign@users.noreply.github.com>
2023-11-05 22:55:49 +0900

    [level 2] Title: 땅따먹기, Time: 196.66 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,5 +1,7 @@
 def solution(land):
-    for i in range(1, len(land)):
-        for j in range(4):
-            land[i][j] += max(land[i -1][: j] + land[i - 1][j + 1:])
-    return max(land[-1])
+    answer = 0
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
