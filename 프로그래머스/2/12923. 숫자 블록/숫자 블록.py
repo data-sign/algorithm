01b10143f9d9f1b4661134f5715e33ef7b003471
data-sign <77310679+data-sign@users.noreply.github.com>
2023-11-06 00:39:49 +0900

    [level 2] Title: 숫자 블록, Time: 1313.65 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.py
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.py
@@ -1,16 +1,3 @@
 def solution(begin, end):
     answer = []
-    #약수 중 가장 큰 수 찾기
-    for i in range(begin,end+1):
-        if i>1:
-            a=1
-            for j in range(2,int(i**(1/2))+1):
-                if i%j==0:
-                    a=j
-                    if i//j<=10000000:
-                        a=i//j
-                        break
-            answer.append(a)
-        elif i==1:
-            answer.append(0)
     return answer
