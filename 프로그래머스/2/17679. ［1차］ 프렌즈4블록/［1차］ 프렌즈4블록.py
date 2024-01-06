ff78570a1ed1f010c88e2cfa0dafb19952fe97d6
data-sign <77310679+data-sign@users.noreply.github.com>
2024-01-06 23:41:54 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.58 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,38 +1,3 @@
 def solution(m, n, board):
     answer = 0
-    board = [list(x) for x in board]
-    
-    tmp = set()
-    while True:
-        
-        for i in range(m-1) :
-            for j in range(n-1):
-                t = board[i][j]
-                if ( t == [] ) :
-                    continue
-                else :
-                    if( board[i][j+1] == t and board[i+1][j] == t and board[i+1][j+1] == t) :
-                        tmp.add((i,j))
-                        tmp.add((i,j+1))
-                        tmp.add((i+1,j))
-                        tmp.add((i+1,j+1))
-                        
-        if ( tmp ) :
-            answer += len(tmp)
-            for i,j in tmp :
-                board[i][j] = []
-            tmp = set()
-        else :
-            break
-        
-        while True :
-            moved = 0
-            for i in range(m-1) :
-                for j in range(n) :
-                    if( board[i][j] and board[i+1][j] == [] ) :
-                        board[i][j],board[i+1][j] = board[i+1][j],board[i][j]
-                        moved = 1
-            if ( moved == 0 ):
-                break
-    
     return answer
