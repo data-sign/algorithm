e39f507af28b799dec096479e9b22d6848a8389c
data-sign <77310679+data-sign@users.noreply.github.com>
2024-10-20 09:47:10 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 80 ms, Memory: 35320 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,26 +1,26 @@
 from collections import deque
 
 def bfs(n, k):
-    q = deque([(n, 0)])  # (위치, 시간)
+    q = deque([n])  # 위치만 저장
     visited = [-1] * 100001  # 방문 여부와 시간을 동시에 체크
     visited[n] = 0
 
     while q:
-        pos, time = q.popleft()
+        pos = q.popleft()
 
         if pos == k:
-            return time
+            return visited[pos]
 
         # 순간이동
-        if 0 <= pos * 2 <= 100000 and (visited[pos * 2] == -1 or visited[pos * 2] > time):
-            q.appendleft((pos * 2, time))  # 시간 증가 없이 맨 앞에 추가
-            visited[pos * 2] = time
+        if 0 <= pos * 2 <= 100000 and visited[pos * 2] == -1:
+            q.appendleft(pos * 2)  # 순간이동은 큐의 앞에 추가
+            visited[pos * 2] = visited[pos]  # 시간 증가 없음
 
         # 걷기 (앞으로, 뒤로)
         for next_pos in (pos - 1, pos + 1):
-            if 0 <= next_pos <= 100000 and (visited[next_pos] == -1 or visited[next_pos] > time + 1):
-                q.append((next_pos, time + 1))
-                visited[next_pos] = time + 1
+            if 0 <= next_pos <= 100000 and visited[next_pos] == -1:
+                q.append(next_pos)
+                visited[next_pos] = visited[pos] + 1
 
 n, k = map(int, input().split())
 print(bfs(n, k))
