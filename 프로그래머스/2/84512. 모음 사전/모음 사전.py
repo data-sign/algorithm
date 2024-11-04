53f26c945aecd2c6412ea812490668ff16f89305
data-sign <77310679+data-sign@users.noreply.github.com>
2024-11-04 10:13:48 +0900

    [level 2] Title: 모음 사전, Time: 7.78 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,13 @@
+def solution(word):
+    lst = ['A', 'E', 'I', 'O', 'U']
+    from itertools import product
+    dictionary = []
+    for i in range(1, 6): 
+        pm = product(lst, repeat=i)
+        for j in pm: 
+            print(j)
+            dictionary.append(''.join(j))
+
+    dictionary = sorted(list(set(dictionary)))
+    answer = dictionary.index(word) + 1
+    return answer
