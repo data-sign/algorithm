46519b5987ab61bfb28d98607c5be92ef83d4f5d
data-sign <77310679+data-sign@users.noreply.github.com>
2024-11-08 09:34:23 +0900

    [Gold IV] Title: Yes or yes, Time: 268 ms, Memory: 61852 KB -BaekjoonHub

---
+++ 백준/Gold/25195. Yes or yes/Yes or yes.py
@@ -0,0 +1,36 @@
+# 25195
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100001)
+
+def dfs(now_v):
+    if visited[now_v] or now_v in is_bear:
+        return
+    visited[now_v] = True
+
+    if not graph[now_v]:
+        print("yes")
+        exit(0)
+
+    for next_v in graph[now_v]:
+        if next_v not in is_bear and not visited[next_v]:
+            dfs(next_v)
+            visited[next_v] = False
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+
+    graph = [[] for _ in range(N + 1)]
+    visited = [False] * (N + 1)
+    for _ in range(M):
+        v1, v2 = map(int, input().split())
+        graph[v1].append(v2)
+
+    S = int(input())
+    is_bear = {}
+    s = list(map(int, input().split()))
+    for _s in s:
+        is_bear[_s] = True
+
+    dfs(1)
+    print("Yes")
