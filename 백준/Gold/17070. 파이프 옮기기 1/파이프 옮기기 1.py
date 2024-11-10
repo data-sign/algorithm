0c2409346a9462d707987d30918f60a562204002
data-sign <77310679+data-sign@users.noreply.github.com>
2024-11-10 12:02:28 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,59 +1,34 @@
-import sys
-input=sys.stdin.readline
-sys.setrecursionlimit(1_000_000)
 n = int(input())
-graph = []
-for i in range(n): 
-    graph.append(list(map(int, input().split())))
-# print(*graph, sep='\n')
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-# 메모이제이션을 위한 배열 : (x, y, direction)
-memo = [[[-1] * 3 for _ in range(n)] for _ in range(n)]
+# dp[x][y][direction]: (x, y) 위치에서 direction 방향으로 놓인 파이프의 경우의 수
+dp = [[[0] * 3 for _ in range(n)] for _ in range(n)]
 
-def move_pipe(x, y, direction, grid, N):
+# 초기 상태: (0, 1) 위치에 수평으로 놓인 파이프
+dp[0][1][0] = 1
 
-    # print(direction, grid, x, y)
-    # 기저 조건: 파이프가 목표 위치에 도달했을 때
-    if x == N - 1 and y == N - 1:
-        # print("count 증가")
-        return 1  # 경우의 수 1 증가
+for x in range(n):
+    for y in range(n):
+        # 수평 방향
+        if y + 1 < n and grid[x][y + 1] == 0:  # 오른쪽으로 이동 가능
+            dp[x][y + 1][0] += dp[x][y][0]  # 수평 유지
+        if x + 1 < n and y + 1 < n and grid[x + 1][y + 1] == 0 and grid[x][y + 1] == 0 and grid[x + 1][y] == 0:
+            dp[x + 1][y + 1][2] += dp[x][y][0]  # 대각선으로 이동
 
-    # 이미 계산된 경우의 수가 있다면 반환
-    if memo[x][y][direction] != -1:
-        return memo[x][y][direction]
-    
-    count = 0  # 경우의 수 초기화
+        # 수직 방향
+        if x + 1 < n and grid[x + 1][y] == 0:  # 아래로 이동 가능
+            dp[x + 1][y][1] += dp[x][y][1]  # 수직 유지
+        if x + 1 < n and y + 1 < n and grid[x + 1][y + 1] == 0 and grid[x][y + 1] == 0 and grid[x + 1][y] == 0:
+            dp[x + 1][y + 1][2] += dp[x][y][1]  # 대각선으로 이동
 
-    # 현재 방향에 따라 다음 위치로 이동
-    if direction == 0:  # 수평
-        # 수평으로 이동
-        if y + 1 < N and grid[x][y + 1] == 0:  # 오른쪽으로 이동 가능
-            count += move_pipe(x, y + 1, 0, grid, N)  # 수평 유지
-        # 대각선으로 이동
-        if x + 1 < N and y + 1 < N and grid[x + 1][y + 1] == 0 and grid[x][y + 1] == 0 and grid[x + 1][y] == 0:
-            count += move_pipe(x + 1, y + 1, 2, grid, N)  # 대각선으로 이동
+        # 대각선 방향
+        if y + 1 < n and grid[x][y + 1] == 0:  # 오른쪽으로 이동 가능
+            dp[x][y + 1][0] += dp[x][y][2]  # 수평으로 이동
+        if x + 1 < n and grid[x + 1][y] == 0:  # 아래로 이동 가능
+            dp[x + 1][y][1] += dp[x][y][2]  # 수직으로 이동
+        if x + 1 < n and y + 1 < n and grid[x + 1][y + 1] == 0 and grid[x][y + 1] == 0 and grid[x + 1][y] == 0:
+            dp[x + 1][y + 1][2] += dp[x][y][2]  # 대각선으로 이동
 
-    elif direction == 1:  # 수직
-        # 수직으로 이동
-        if x + 1 < N and grid[x + 1][y] == 0:  # 아래로 이동 가능
-            count += move_pipe(x + 1, y, 1, grid, N)  # 수직 유지
-        # 대각선으로 이동
-        if x + 1 < N and y + 1 < N and grid[x + 1][y + 1] == 0 and grid[x][y + 1] == 0 and grid[x + 1][y] == 0:
-            count += move_pipe(x + 1, y + 1, 2, grid, N)  # 대각선으로 이동
-
-    elif direction == 2:  # 대각선
-        # 수평으로 이동
-        if y + 1 < N and grid[x][y + 1] == 0:  # 오른쪽으로 이동 가능
-            count += move_pipe(x, y + 1, 0, grid, N)  # 수평으로 이동
-        # 수직으로 이동
-        if x + 1 < N and grid[x + 1][y] == 0:  # 아래로 이동 가능
-            count += move_pipe(x + 1, y, 1, grid, N)  # 수직으로 이동
-        # 대각선으로 이동
-        if x + 1 < N and y + 1 < N and grid[x + 1][y + 1] == 0 and grid[x][y + 1] == 0 and grid[x + 1][y] == 0:
-            count += move_pipe(x + 1, y + 1, 2, grid, N)  # 대각선으로 이동
-
-    # 계산한 경우의 수를 메모이제이션 배열에 저장
-    memo[x][y][direction] = count
-    return count  # 경우의 수 반환
-
-print(move_pipe(0, 1, 0, graph, n))
+# 결과 출력: (N-1, N-1) 위치에 도달하는 경우의 수
+result = dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2]
+print(result)
